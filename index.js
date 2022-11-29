6a960b9b991a6409c440def735a9f96687b1b7fd
Kellymugambi <kellymgambi@.com>
2022-11-29 12:54:08 +0300

    2 challenges

---
+++ index.js
@@ -0,0 +1,53 @@
+// 1 STUDENT GRADE CHALLENGE
+
+//0-100 marks
+// 80-100 ="A"
+// 60-79="B"
+// 50-59="C"
+// 40-49="D"
+// less 40="E"
+
+function grade(marks){
+
+    if (marks < 40){
+        return "Grade E"
+    }
+    else if (marks < 50){
+        return "Grade D"
+    }
+    else if(marks < 60){
+        return "Grade C"
+    }
+    else if (marks < 80){
+        return "Grade B"
+    }
+    else if (marks < 100){
+        return "Grade A"
+    }
+}
+// console.log(grade(74))
+
+
+
+// 2 SPEED DETECTOR 
+
+//  if speed is less 70= "OK"
+// if speed is more than 70, for every 5km/s give driver one demerit point
+// if the points exceed 12, print license suspended
+
+function speed(speed){
+    if (speed<70){
+        return "OK"
+    }
+    if (((speed-70)/5)>12)    
+        return "License suspended"
+        
+    if (speed>70){
+        return "Points:"+((speed-70)/5)
+    }
+    }
+// console.log(speed(85))
+
+
+// Net salary calculator
+
